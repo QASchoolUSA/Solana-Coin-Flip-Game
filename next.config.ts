aec90a0c01183f46e0896438a09264da4a897ea3
QASchoolUSA <64873838+QASchoolUSA@users.noreply.github.com>
2024-12-22 23:02:23 -0500

    Added envs to next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 require('dotenv').config();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_HOUSE_WALLET_SECRET: process.env.NEXT_PUBLIC_HOUSE_WALLET_SECRET,
+    NEXT_PUBLIC_PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+  }
 };
 
 export default nextConfig;
