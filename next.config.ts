fd224c6af20ea692529cfe80756b8843a5f7ac3d
QASchoolUSA <64873838+QASchoolUSA@users.noreply.github.com>
2024-12-23 04:58:52 -0500

    Trying to fix an error with images missing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 require('dotenv').config();
 
 const nextConfig: NextConfig = {
+  basePath: "Solana-Coin-Flip-Game",
   env: {
     NEXT_PUBLIC_HOUSE_WALLET_SECRET: process.env.NEXT_PUBLIC_HOUSE_WALLET_SECRET,
     NEXT_PUBLIC_PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY,
