e7a6b22be7f456cc4ddc4d3d45a2874b218c075d
QASchoolUSA <64873838+QASchoolUSA@users.noreply.github.com>
2024-12-23 05:04:27 -0500

    Added new changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 require('dotenv').config();
 
 const nextConfig: NextConfig = {
-  basePath: "Solana-Coin-Flip-Game",
+  basePath: '/Solana-Coin-Flip-Game',
+  assetPrefix: '/Solana-Coin-Flip-Game',
   env: {
     NEXT_PUBLIC_HOUSE_WALLET_SECRET: process.env.NEXT_PUBLIC_HOUSE_WALLET_SECRET,
     NEXT_PUBLIC_PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY,
